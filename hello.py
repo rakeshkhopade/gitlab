21a6c344146e334bba71945ebf925208a6ae8122
rakeshkhopade <rakeshkhopade12396@gmail.com>
2022-06-29 15:50:17 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ print("hello")
 print("hello once again")
 print("hello once again 2")
 print("hopefully this works fine")
+<<<<<<< HEAD
 print("see if release works fine")
 input("enter 1")
 input
-
